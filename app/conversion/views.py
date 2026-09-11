5d558044ba4812c286eeda7cb6f15cf81f110926
mquirosq <mquirosquiroga@gmail.com>
2026-09-11 15:11:14 +0200

    chore: cleaned inputs

--- app/conversion/views.py
+++ app/conversion/views.py
@@ -4,7 +4,7 @@ import os
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import FileResponse, HttpResponse, request
+from django.http import FileResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
